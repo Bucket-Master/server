1caab2fceac28aedc2b0aa801c7ec9f773009b77
edurodrigues0 <edusacramento9@gmail.com>
2024-05-03 20:03:47 -0300

    adicinando contratos no TaskRepository

--- src/repositories/tasks-repository.ts
+++ src/repositories/tasks-repository.ts
@@ -1,5 +1,11 @@
 import { Prisma, Task } from '@prisma/client'
 
+import { Pagination } from '@/@types/pagination'
+
 export interface TasksRepository {
   create(data: Prisma.TaskCreateInput): Promise<Task>
+  edit(taskId: string, data: Prisma.TaskUncheckedUpdateInput): Promise<Task>
+  findById(taskId: string): Promise<Task | null>
+  fetch(page: number): Promise<Pagination>
+  delete(taskId: string): Promise<void>
 }
